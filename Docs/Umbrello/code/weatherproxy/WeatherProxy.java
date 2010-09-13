c859b9273cc6df1a6b073453042a1f7731ee62b0
tibnor <torsteinibo@gmail.com>
2010-09-13 22:02:47 +0200

    Added a image of arcitecture

---
+++ Docs/Umbrello/code/weatherproxy/WeatherProxy.java
@@ -0,0 +1,33 @@
+package weatherproxy;
+
+
+/**
+ * Interface WeatherProxy
+ */
+public interface WeatherProxy {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       Cursor
+   */
+  public Cursor query(  );
+
+
+}
---
+++ Docs/Umbrello/code/weatherproxy/YrProxy.java
@@ -0,0 +1,40 @@
+package weatherproxy;
+
+
+/**
+ * Class YrProxy
+ */
+class YrProxy implements WeatherProxy {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public YrProxy () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       android.database.Cursor
+   */
+  public android.database.Cursor query(  )
+  {
+  }
+
+
+}
