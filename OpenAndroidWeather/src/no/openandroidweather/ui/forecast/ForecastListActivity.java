0a00917263268b4274674a09ac60874450888d33
tibnor <torsteinibo@gmail.com>
2010-09-14 21:32:06 +0200

    Remade the list view, now it is a scrollable table view.

--- OpenAndroidWeather/src/no/openandroidweather/ui/forecast/ForecastListActivity.java
+++ OpenAndroidWeather/src/no/openandroidweather/ui/forecast/ForecastListActivity.java
@@ -19,37 +19,42 @@
 
 package no.openandroidweather.ui.forecast;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Formatter;
 
 import no.openandroidweather.R;
 import no.openandroidweather.misc.IProgressItem;
 import no.openandroidweather.weathercontentprovider.WeatherContentProvider;
+import no.openandroidweather.weathercontentprovider.WeatherContentProvider.ForecastListView;
 import no.openandroidweather.weathercontentprovider.WeatherContentProvider.Place;
 import no.openandroidweather.weatherservice.IForecastEventListener;
 import no.openandroidweather.weatherservice.IWeatherService;
 import no.openandroidweather.weatherservice.WeatherService;
+import no.openandroidweather.widget.Q;
+import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
-import android.widget.HeaderViewListAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.ListView.FixedViewInfo;
-import android.widget.ProgressBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
-public class ForecastListActivity extends ListActivity implements IProgressItem {
+public class ForecastListActivity extends Activity implements IProgressItem {
 	/**
 	 * Set this in intent extra to show a specific place. Use row id from
 	 * WeatherContentProvider.Places
@@ -57,9 +62,12 @@ public class ForecastListActivity extends ListActivity implements IProgressItem 
 	public static final String PLACE_ROW_ID = "_rowId";
 	private final String TAG = "ForecastListActivity";
 	private final IForecastEventListener forcastListener = new ForecastListener();
-	private ProgressBar progressBar;
+	private ProgressDialog progressBar;
+	private TableLayout mTable;
 	private final Handler mHandler = new Handler();
 	private IWeatherService mService = null;
+	private LayoutInflater inflater;
+	final DateFormat df = DateFormat.getDateInstance(DateFormat.FULL);
 
 	private final ServiceConnection mServiceConnection = new ServiceConnection() {
 
@@ -127,9 +135,20 @@ public class ForecastListActivity extends ListActivity implements IProgressItem 
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.forecast_view);
-		progressBar = (ProgressBar) findViewById(R.id.progressbar);
+		// progressBar = (ProgressBar) findViewById(R.id.progressbar);
+		mTable = (TableLayout) findViewById(R.id.forecastsTable);
+		mTable.setStretchAllColumns(true);
 		bindService(new Intent(getApplicationContext(), WeatherService.class),
 				mServiceConnection, BIND_AUTO_CREATE);
+
+		inflater = getLayoutInflater();
+		
+		progressBar = new ProgressDialog(this);
+		progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		progressBar.setMax(1000);
+		progressBar.setMessage(getResources().getText(R.string.please_wait_1min));
+		progressBar.setCancelable(false);
+		progressBar.show();
 
 	}
 
@@ -152,6 +171,205 @@ public class ForecastListActivity extends ListActivity implements IProgressItem 
 	@Override
 	public void progress(final int progress) {
 		progressBar.setProgress(progress);
+	}
+
+	private void checkDate(final long from, final long to) {
+		final Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(from);
+		final int oldDate = cal.get(Calendar.DATE);
+		cal.setTimeInMillis(to);
+		final int newDate = cal.get(Calendar.DATE);
+		if (oldDate != newDate)
+			inflateDateRow(to);
+
+	}
+
+	public void inflateDateRow(long date) {
+		final View row = inflater.inflate(R.layout.forecast_view_date_item,
+				null);
+		final DateFormat df = DateFormat.getDateInstance(DateFormat.FULL);
+		String dateS = df.format(new Date(date));
+		((TextView) row).setText(dateS);
+		runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				mTable.addView(row);
+			}
+		});
+	}
+
+	/** Get the meta data and render the header
+	 * 
+	 * @param uri
+	 * @return If no forecast it returns false else true
+	 */
+	public boolean setHeader(final Uri uri) {
+		final Cursor c = getContentResolver()
+				.query(uri, null, null, null, null);
+		if (c == null || c.getCount() == 0){
+			c.close();
+			noForecast();
+			return false;
+		}
+
+		c.moveToFirst();
+		final DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.SHORT);
+
+		final String place = c.getString(c
+				.getColumnIndex(WeatherContentProvider.Meta.PLACE_NAME));
+		final long downloaded = c.getLong(c
+				.getColumnIndex(WeatherContentProvider.Meta.LOADED));
+		final long generated = c.getLong(c
+				.getColumnIndexOrThrow(WeatherContentProvider.Meta.GENERATED));
+		final long nextForecast = c
+				.getLong(c
+						.getColumnIndexOrThrow(WeatherContentProvider.Meta.NEXT_FORECAST));
+		c.close();
+
+		runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				((TextView) findViewById(R.id.place)).setText(place);
+
+				((TextView) findViewById(R.id.downloaded)).setText(df
+						.format(new Date(downloaded)));
+
+				((TextView) findViewById(R.id.generated)).setText(df
+						.format(new Date(generated)));
+
+				((TextView) findViewById(R.id.next_update)).setText(df
+						.format(new Date(nextForecast)));
+			}
+		});
+		return true;
+
+	}
+
+	public void parseData(Uri uri, View mainView) {
+		Uri forecastUri = Uri.withAppendedPath(uri,
+				ForecastListView.CONTENT_PATH);
+
+		if(!setHeader(uri))
+			return;
+
+		// Set start time to the beginning of this hour
+		long lastHour = System.currentTimeMillis();
+		lastHour -= 3600000;
+
+		final String selection = ForecastListView.fromTime + ">" + lastHour;
+		final Cursor c = getContentResolver().query(forecastUri, null,
+				selection, null, null);
+
+		if (c == null || c.getCount() == 0) {
+			c.close();
+			noForecast();
+			return;
+		}
+
+		final int fromCol = c.getColumnIndexOrThrow(ForecastListView.fromTime);
+		final int toCol = c.getColumnIndexOrThrow(ForecastListView.toTime);
+		final int percipitationCol = c
+				.getColumnIndexOrThrow(ForecastListView.percipitation);
+		final int symbolCol = c.getColumnIndexOrThrow(ForecastListView.symbol);
+		final int temperatureCol = c
+				.getColumnIndexOrThrow(ForecastListView.temperature);
+		final int windDirectionCol = c
+				.getColumnIndexOrThrow(ForecastListView.windDirection);
+		final int windSpeedCol = c
+				.getColumnIndexOrThrow(ForecastListView.windSpeed);
+
+		c.moveToFirst();
+		long from = c.getLong(fromCol), to;
+		int symbol;
+		double percipitation, temperature, windDirection, windSpeed;
+		inflateDateRow(from);
+		int length = c.getCount();
+		for (int i = 0; i < length; i++) {
+			from = c.getLong(fromCol);
+			to = c.getLong(toCol);
+			symbol = c.getInt(symbolCol);
+			percipitation = c.getDouble(percipitationCol);
+			temperature = c.getDouble(temperatureCol);
+			windDirection = c.getDouble(windDirectionCol);
+			windSpeed = c.getDouble(windSpeedCol);
+			addForecastRow(symbol, temperature, percipitation, windSpeed,
+					windDirection, from);
+			checkDate(from, to);
+			c.moveToNext();
+
+		}
+		c.close();
+
+		return;
+	}
+
+	private void noForecast() {
+		final AlertDialog.Builder builder = new AlertDialog.Builder(
+				ForecastListActivity.this);
+		builder.setMessage(R.string.no_forecast);
+		builder.setCancelable(false);
+		builder.setPositiveButton(getResources().getString(R.string.ok),
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(final DialogInterface dialog,
+							final int which) {
+						ForecastListActivity.this.finish();
+						dialog.cancel();
+					}
+				});
+		final Thread ShowErrorThread = new Thread() {
+			@Override
+			public void run() {
+				builder.show();
+			};
+		};
+		mHandler.post(ShowErrorThread);
+	}
+
+	public void addForecastRow(final int symbol, final double temperature,
+			final double percipitation, final double windSpeed,
+			final double windDirection, final long startTime) {
+		final TableRow row = (TableRow) inflater.inflate(
+				R.layout.forecast_view_item, null);
+		final Formatter formatter = new Formatter();
+		String hour = formatter.format("%tH", startTime).toString();
+
+		// Set hour
+		((TextView) row.findViewById(R.id.hour)).setText(hour);
+
+		// Set symbol
+		((ImageView) row.findViewById(R.id.symbol))
+				.setImageResource(Q.symbol[symbol]);
+
+		// Set temperature
+		((TextView) row.findViewById(R.id.temperature)).setText(temperature
+				+ "°C");
+
+		// Set precipitation
+		((TextView) row.findViewById(R.id.percipitation)).setText(percipitation
+				+ " mm");
+
+		// Set wind speed
+		((TextView) row.findViewById(R.id.wind_speed)).setText(windSpeed
+				+ " m/s");
+
+		// Set wind direction
+		((TextView) row.findViewById(R.id.wind_direction))
+				.setText(windDirection + "°");
+
+		runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				mTable.addView(row);
+			}
+		});
+
+		return;
 	}
 
 	private class ForecastListener extends IForecastEventListener.Stub {
@@ -232,57 +450,17 @@ public class ForecastListActivity extends ListActivity implements IProgressItem 
 		@Override
 		public void newForecast(final String uri, final long forecastGenerated)
 				throws RemoteException {
-			ForecastListActivity.this.progress(500);
 			hasGotForecast = true;
-			new RenderContentTask().execute(Uri.parse(uri));
+			progressBar.setProgress(1000);
+			progressBar.dismiss();
+			parseData(Uri.parse(uri), findViewById(R.id.main));
+
 		}
 
 		@Override
 		public void progress(final int progress) throws RemoteException {
 			if (!hasGotForecast)
-				ForecastListActivity.this.progress(progress / 2);
-		}
-
-	}
-
-	private class RenderContentTask extends AsyncTask<Uri, Float, ListAdapter> {
-
-		@Override
-		protected ListAdapter doInBackground(final Uri... params) {
-			final ForecastListParser parser = new ForecastListParser(
-					getApplicationContext(), ForecastListActivity.this);
-			final List<IListRow> rows = parser.parseData(params[0]);
-			if (rows == null || rows.isEmpty())
-				return null;
-
-			final ListView.FixedViewInfo header = getListView().new FixedViewInfo();
-			header.view = parser.getHeaderView(params[0]);
-			final ArrayList<FixedViewInfo> headerViewInfos = new ArrayList<ListView.FixedViewInfo>();
-			headerViewInfos.add(header);
-			return new HeaderViewListAdapter(headerViewInfos, null,
-					new ForecastListAdapter(rows));
-		}
-
-		@Override
-		protected void onPostExecute(final ListAdapter result) {
-			super.onPostExecute(result);
-			if (result != null)
-				setListAdapter(result);
-			else {
-				AlertDialog.Builder builder = new AlertDialog.Builder(
-						ForecastListActivity.this);
-				builder.setMessage(R.string.no_forecast)
-						.setCancelable(false)
-						.setPositiveButton(R.string.ok,
-								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int id) {
-										ForecastListActivity.this.finish();
-									}
-								});
-				AlertDialog alert = builder.create();
-				alert.show();
-			}
+				ForecastListActivity.this.progress(progress);
 		}
 
 	}
